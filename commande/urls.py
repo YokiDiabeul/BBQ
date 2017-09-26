aa4264d0596d41c1c2c60f9de4aa3f5391d2d648
Unknown <audren.guillaume@student.hers.be>
2017-09-26 22:20:54 +0200

    Views v01

--- commande/urls.py
+++ commande/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'commande'
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(),name='login'),
+    url(r'^$', views.IndexView.as_view(),name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='commande'),
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='admin'),
 ]
--- commande/views.py
+++ commande/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import loader
+from django.utils import timezone
+from django.urls import reverse
+from django.views import generic
+
+from .models import BbqParty, Commande, Commande, Miam, Participant, Stuff
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the commande index.")
+class IndexView(generic.ListView):
+    template_name = 'index.html'
+    context_object_name = '??????'
+    return HttpResponseRedirect(reverse('commande:?????', args=(question.id,)))
+
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the commande index.")
