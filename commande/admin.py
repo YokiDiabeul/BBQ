5f38ecd6820c486cb046ab5cf2ac3f0940276e6d
YokiDiabeul <diabeul@live.fr>
2017-09-26 23:00:18 +0200

    Auto stash before merge of "master" and "master/master"
    lmmlk

--- commande/admin.py
+++ commande/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.CommandeTotal)
+class CommandeTotalAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Nourriture)
+class NourritureAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Boisson)
+class BoissonAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Stuff)
+class StuffAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    pass
