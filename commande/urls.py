32c14f4c8c5bb1880371239365a2b3811ef119f1
Unknown <audren.guillaume@student.hers.be>
2017-09-26 22:10:56 +0200

    URL Pattern

--- commande/urls.py
+++ commande/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 app_name = 'commande'
 urlpatterns = [
+    url(r'^$', views.login, name='login'),
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='commande'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='admin'),
 ]
