1bf91b1178d739b3f40d4721d598397cf479a45f
YokiDiabeul <diabeul@live.fr>
2017-09-27 16:03:08 +0200

    Auto stash before merge of "master" and "master/master"
    piti test

--- commande/tests.py
+++ commande/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Produit, Commande, Participant, Evenement
+
+
+
+# Test sur les fonctio dans les models
+class ProduitMethodTest(TestCase):
+
+    def testCalculerPrix_withPrixNegatif(self):
+        """
+        La fonction CaclulerPrix doit retourner 0 si prix est négatif
+        """
+
+        produit = Produit.objects.create("Boeuf", prix=-1, 0, 'v')
+        self.assertEqual(produit.calculerPrix(), 0)
+
+
+
+# Test sur les views
+class LoginViewTest(TestCase):
+    
